d796719c88339611531fbcece3a3a33dee6ca111
notSaqlain <iamnotsaqlain@gmail.com>
2025-01-17 09:49:16 +0100

    feat: Add calcolaTotale method to Spesa class

--- Spesa.java
+++ Spesa.java
@@ -3,27 +3,34 @@ import java.util.HashMap;
 public class Spesa {
     private HashMap<String, Prodotto> listaProdotti;
 
-    // Constructor
+    
     public Spesa() {
         this.listaProdotti = new HashMap<>();
     }
-
-    // Method to add a product to the shopping list
+    
     public void aggiungiProdotto(Prodotto prodotto) {
         listaProdotti.put(prodotto.getCodiceABarre(), prodotto);
     }
 
-    // Method to remove a product from the shopping list
+    
     public void rimuoviProdotto(String codiceABarre) {
         listaProdotti.remove(codiceABarre);
     }
 
-    // Method to get a product from the shopping list
+    
     public Prodotto getProdotto(String codiceABarre) {
         return listaProdotti.get(codiceABarre);
     }
 
-    // Method to print all products in the shopping list
+    public int calcolaTotale() {
+        int totale = 0;
+        for (Prodotto prodotto : listaProdotti.values()) {
+            totale += prodotto.getCosto() * prodotto.getQuantita();
+        }
+        return totale;
+    }
+
+    
     public void stampaListaProdotti() {
         for (Prodotto prodotto : listaProdotti.values()) {
             System.out.println("Codice a Barre: " + prodotto.getCodiceABarre());
